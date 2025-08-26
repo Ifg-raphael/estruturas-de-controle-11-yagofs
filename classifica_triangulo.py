b7035281dde371ddf07b0693347e7f1e9955085f
Yagofs <yagofsarmento8@gmail.com>
2025-08-26 14:52:37 -0300

    Solução

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,13 @@
-# Sua solução aqui
+nome = ["a", "b", "c"]
+valores = []
+for numero in nome:
+    valor = int(input(f"Digite o valor de {numero}: "))
+    valores.append(valor)
+if len(set(valores)) == 1:
+    print ("é um triângulo equilátero")
+elif len(set(valores)) == 2:
+    print ("é um triângulo isósceles")
+elif len(set(valores)) == 3:
+    print ("é um triângulo escaleno")
+else:
+    print ("não é um triângulo")
